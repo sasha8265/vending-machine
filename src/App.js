d2137374ef12d71a145986a7e7a621e54afb0c77
Sasha Czerniawski <sashacandoit@gmail.com>
2022-12-23 12:00:28 -0500

    first route showing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+// import ReactDOM from "react-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import VendingMachine from "./VendingMachine"
 
 function App() {
   return (
-    <div className="App">
-      <VendingMachine />
+      <div className="App">
+          <BrowserRouter>
+              <Routes>
+                  <Route
+                      exact path='/'
+                      element={<VendingMachine />}
+                  />
+              </Routes>
+          </BrowserRouter>
+      
     </div>
   );
 }
