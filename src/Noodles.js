ba0318d1c496248540a63a62b86d615cbdc650b5
Sasha Czerniawski <sashacandoit@gmail.com>
2022-12-23 12:59:02 -0500

    added some styling

--- src/Noodles.js
+++ src/Noodles.js
@@ -4,7 +4,7 @@ import "./Noodles.css"
 const Noodles = () => {
     return (
         <div className="Noodles">
-            <h1 className="Noodles-title">NOODLES</h1>
+            <h2 className="Noodles-title">NOODLES</h2>
             <img className="Noodles-img"
                 src="https://media2.giphy.com/media/WtVL452Yfaksck8ZoF/200w.gif?cid=6c09b952g76i0nd5yky5e0pddav6hngmw6234xnl2s8upvu3&rid=200w.gif&ct=g"
             alt="noodle bowl"
